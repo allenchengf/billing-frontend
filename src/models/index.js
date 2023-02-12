c1bd67f1d55de26c21477de0c8b1c30d3c3dc972
willy <willy8742891@gmail.com>
2023-02-12 19:14:18 +0800

    feat: add models.

---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+/**
+ * @typedef {Object} TokenData
+ * @property {number} pk
+ * @property {string} token
+ */
+/**
+ * @typedef {Object} CustomerModel
+ * @property {number} id
+ * @property {string} customer_name
+ * @property {string} customer_id
+ * @property {string} customer_property
+ * @property {string} am
+ * @property {string} [description]
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+/**
+ * @typedef {Object} SubscriptionModel
+ * @property {number} id
+ * @property {number} customer_id
+ * @property {string} product
+ * @property {string} service_id
+ * @property {string} poc
+ * @property {string} status
+ * @property {string} description
+ * @property {string} created_at
+ * @property {string} updated_at
+ */
+
+export {}
