03738e697afd17ce74f35d8deb4c3a44fd72f8fc
Allen Chen <allen.chen@hiero7.com>
2023-02-17 15:40:02 +0800

    rename

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
       <div class="title-container">
-        <h3 class="title">Login Form</h3>
+        <h3 class="title">Login</h3>
       </div>
 
       <el-form-item prop="username">
@@ -69,8 +69,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'rdadmin',
-        password: '1qaz@WSX'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
